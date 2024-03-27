f324594156815ceeab9cc6b2e9c0a767a50baecc
arjit.garg <arjit.garg@sap.com>
2024-03-27 10:54:19 +0530

    Initial Commit

---
+++ src/test/java/com/psg/mavenProject/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psg.mavenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
